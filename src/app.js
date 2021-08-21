fd82e530e4f00e51f94ede0a9c24c08db899c5a0
ch4rbo <baueratctu@gmail.com>
2021-08-21 17:24:25 +0000

    Sigo definiendo variables y arrays

--- src/app.js
+++ src/app.js
@@ -1,18 +1,25 @@
-let who = ['El Perro','Mi Abuela','Su tortuga','Mi pajaro'];
-let action = ['comió','orinó','aplastó','rompió'];
+let who = ['El Perro', 'Mi Abuela', 'Su tortuga', 'Mi pajaro'];
+let action = ['comió', 'orinó', 'aplastó', 'rompió'];
 let what = ['mi tarea', 'las llaves', 'mi ferrari'];
-let when = ['antes de clases','justo a tiempo','cuando dormía'];
+let when = ['antes de clases', 'justo a tiempo', 'cuando dormía'];
 
-function theExcuseGenerator(){
+function theExcuseGenerator() {
   who.length
   action.length
   what.length
   when.length
-
-
 }
 
 
+function excuse(array1, array2, array3, array4) {
+  let whoRandomNumber = Math.floor(Math.random() * array1.length);
+  let actionWhoRandomNumber = Math.floor(Math.random() * array2.length);
+  let whatRandomNumber = Math.floor(Math.random() * array3.length);
+  let whenRandomNumber = Math.floor(Math.random() * array4.length);
 
-
-Math.random(Math.random()
+  console.log(
+    whoRandomNumber,
+    actionWhoRandomNumber,
+    whatRandomNumber,
+    whenRandomNumber
+  );
