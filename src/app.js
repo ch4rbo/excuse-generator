773b43f3683162e6bb8b36b396e5d952596245be
ch4rbo <baueratctu@gmail.com>
2021-08-23 23:56:32 +0000

    Te odio java

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
-let who = ['El Perro', 'Mi Abuela', 'Su tortuga', 'Mi pajaro'];
-let action = ['comió', 'orinó', 'aplastó', 'rompió'];
-let what = ['mi tarea', 'las llaves', 'mi ferrari'];
-let when = ['antes de clases', 'justo a tiempo', 'cuando dormía'];
+import "bootstrap";
+import "./style.css";
 
-function theExcuseGenerator() {
-  who.length
-  action.length
-  what.length
-  when.length
-}
+let who = ["El Perro", "Mi Abuela", "Su tortuga", "Mi pajaro"];
+let action = ["comió", "orinó", "aplastó", "rompió"];
+let what = ["mi tarea", "las llaves", "mi ferrari"];
+let when = ["antes de clases", "justo a tiempo", "cuando dormía"];
 
+window.onload = function() {
+  let excusa = excuse(who, action, what, when);
+  let excuseElement = document.getElementById("excuse");
+  excuseElement.innerHTML = excusa;
+};
 
 function excuse(array1, array2, array3, array4) {
   let whoRandomNumber = Math.floor(Math.random() * array1.length);
-  let actionWhoRandomNumber = Math.floor(Math.random() * array2.length);
+  let actionRandomNumber = Math.floor(Math.random() * array2.length);
   let whatRandomNumber = Math.floor(Math.random() * array3.length);
   let whenRandomNumber = Math.floor(Math.random() * array4.length);
 
-  console.log(
-    whoRandomNumber,
-    actionWhoRandomNumber,
-    whatRandomNumber,
-    whenRandomNumber
+  return (
+    array1[whoRandomNumber] +
+    " " +
+    array2[actionRandomNumber] +
+    " " +
+    array3[whatRandomNumber] +
+    " " +
+    array4[whenRandomNumber]
   );
+}
